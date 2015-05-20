9fd94904b5eb6204aeb5db9ac77e9e70c3985825
Zeno Zeng <zenoofzeng@gmail.com>
2015-05-21 01:45:10 +0800

    update demo

--- demo.js
+++ demo.js
@@ -1,35 +1,80 @@
 function decisionTree(examples, attrs, values) {
+    var root = {children: [], examples: examples, selector: "*"};
     var iter = function(examples, attrs, node) {
         if (examples.every(function(example) {return example.playTennis;})) {
-            return {label: '+'};
+            node.children.push({selector: '+'});
         } else if (examples.every(function(example) {return !example.playTennis;})) {
-            return {label: '-'};
+            node.children.push({selector: '-'});
         } else if (attrs.length == 0){
             return null;
         } else {
             var attr = attrs.shift();
             Object.keys(values[attr]).forEach(function(k) {
                 var val = values[attr][k];
-                node[attr + "=" + val] = iter(examples.filter(function(el) {
+                var selector = attr + "=" + val;
+                var subexamples = examples.filter(function(el) {
                     return el[attr] == val;
-                }), attrs, {});
+                });
+                var child = {selector: selector, examples: subexamples, children: []};
+                iter(subexamples, attrs, child);
+                node.children.push(child);
             });
             return node;
         }
     };
-    return iter(examples, attrs, {});
+    return iter(examples, attrs, root);
 }
 
-function drawTree(tree) {
+function tree2line(tree) {
+    var lines = [];
+    var iter = function(node, line) {
+        if (!lines[line]) {
+            lines[line] = [];
+        }
+        lines[line].push(node);
+        if (!node.children) return;
+        node.children.forEach(function(child) {
+            iter(child, line + 1);
+        });
+    };
+    iter(tree, 0);
+    return lines;
 }
 
 function setup() {
-    createCanvas(800, 400);
+    noCanvas();
+    fill(0);
+    background(245);
+    textAlign(CENTER);
 }
 
 function draw() {
     noLoop();
-    background(200);
     var tree = decisionTree(window.data, ["outlook", "temperature", "humidity", "wind"], window.values);
+    var iter = function(tree) {
+        if (tree.examples) {
+            tree.play = tree.examples.filter(function(el) { return el.playTennis; }).length;
+            tree.dontPlay = tree.examples.filter(function(el) { return !el.playTennis; }).length;
+            // tree.examples = null;
+        }
+        if (tree.children) {
+            tree.children.forEach(iter);
+        }
+    };
+    iter(tree);
     console.log(tree);
+    $('#json').JSONView(tree);
+    // console.log(tree);
+    // console.log(JSON.stringify(tree, null, 4));
+    // var lines = tree2line(tree);
+    // lines.forEach(function(line, y) {
+    //     y /= lines.length;
+    //     y *= height;
+    //     line.forEach(function(el, x) {
+    //         x += 0.5;
+    //         x /= line.length;
+    //         x *= width;
+    //         text(el.selector, x, y);
+    //     });
+    // });
 }
